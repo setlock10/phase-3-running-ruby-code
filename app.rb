47812b5273810b4b1fa48fa226a986c73f8b5f27
John J Setlock II <johnsetlock@gmail.com>
2022-08-27 09:49:35 -0500

    Completed Assignment

---
+++ app.rb
@@ -0,0 +1,14 @@
+# puts "Hello world"
+# puts "Hello world"
+# puts "Hello world"
+# puts [1,2,3]
+# p [1,2,3]
+# pp [1,2,3]
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+puts "Hello World!"
+p "Pass this test, please."
+p [1,2,3]
